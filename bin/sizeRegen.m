da487dabcb3c2e5ff38d40ce975d5e908e852c7c
Andrew-Radulovich <aradulov@purdue.edu>
2022-11-05 10:41:11 -0400

    Update sizeRegen.m
    Added Regen Sizing Heister Procedure

--- bin/sizeRegen.m
+++ bin/sizeRegen.m
@@ -72,3 +72,72 @@ supersonic_area_ratios = (pi * r_contour(x_contour > 0) .^ 2) / A_t;
     % make T_wg guess (gas-side wall temperature)
 
     % calculate h_g (film coefficient) from q_dot = h_g * (T_r - T_wg_guess) - T_r is the "gas temperature" axially along the chamber from CEA
+%% Regen Sizing (Heister Procedure)
+%%Initialization
+stepnum = 100;
+Twgi = 1000; %Initial Guess of Gas side Temperature
+gasheattransfer = 1000;
+liqheatransfer = 0;
+
+%Geometric
+wallthick = 100; %Wall Thickness
+d = 5; %?????? d in num channel equation
+engine_diameter = 10; %????? In num channel equation
+Alocal = []; %Local Cross Sectional Areas of Engine
+A_t = 10;  %Area at throat
+dlocal = [];  %Local Cross Sections of Channels
+
+Dstar = %Diameter at Nozzle Throat
+characteristic_length = 10 %Longest length of Channels 
+
+
+%Coolant properties
+viscosity = 10; %Viscosity
+Cp = 100; %Specific heat at constant pressure
+kc = 100; %Thermal Conductivity of Coolant
+Tl = 293;
+
+%Engine Properties
+Pi = 20; %Inflow Pressure
+Ti = 200; %Inflow Temp
+Tgas = []; %Gas temperature from 1-D 
+mdotf = 30; %Coolant/fuel mass flow
+chamber_pressure = 100; %Chamber Pressure
+Cstar = 1000; %Characteristic Velocity
+throat_radius_curvature = 10; %Throat radius Curvature
+
+kw = 100; %Thermal Conductivity of Wall
+
+
+%Step 2: Calculate Number of channels and channel mass flow rate
+numchannels = pi * (engine_diameter + 0.8 * (Dstar + 2 * wallthick)) / (Dstar + 2 * wallthick); %Number of Channels (EQ 6.30) (Change the coefficient later)
+mdotchan = mdotf / numchannels; %Mass flow of channel (EQ 6.31)
+%Step 3: Begin Stepping Down tube/channel
+for i = [1:stepsize]
+    [~, ~, ~, M, gamma, P, T, rho, mu, Pr, Mw, k, son, cp] = RunCEA(P_c, P_e, fuel, fuel_weight, fuel_temp, oxidizer, oxidizer_temp, OF, 0, 0, CEA_input_name, 1, 0);
+
+    Pr_gas = densityfs * viscosityfs * D;
+    Pr_liquid = density(Tl(i));
+    while liqheattransfer < gasheattransfer * upperbound || liqheattransfer > gasheattransfer * lowerbound
+        %Step 5: Calculate Gas film coefficient and heat transfer 
+        hg = 0.026 / Dstar^0.2 * (viscosity^0.2 * Cp / prandtl^0.6) * ...
+        (chamber_pressure * g / Cstar)^0.8 * (Dstar / throat_radius_curvature)^0.1 * (Astar/Alocal[i])^0.9 * (meandens / freedens)^0.8 * (meanvisc / freevisc)^0.2; %Gas Film Coefficent Bartz 
+        gasheattransfer = heatcoef * (Tgas(i) - Twgi);  %Gas Heat Transfer (EQ 6.16)
+
+        %Step 6: Calculate Liquid Wall Temperature from conduction 
+        Twl(i) = gaswalltemp - gasheattrans * wallthick / thermconduct; %Liquid Wall Temp (EQ 6.29)
+
+        %Step 7: Compute Liquid Film Coefficient
+        hl = (a * re^m * pr^n * (visfree / viscwall)^b) * k / L;    %Liquid Film Coefficient (EQ 6.19) 
+
+        %Step 8: Compute Heat Flux. Compare it to step 5
+        liqheatransfer = hl * (Tl(i) - Twl(i)); %Liquid Heat Transfer (EQ  6.29)
+
+        %Step 4: Guess Gass Wall Temperature
+
+        twg(i) = Tgas(i) - liqheatransfer / hg; %Guess Gass Wall temp using liquid heat transfer value
+
+        %Step 9: Run loop until step 5/8 get same value for heat flux
+    end
+    Tl(i + 1) = T(i) + 1 / (mdotchan * Cp);
+
