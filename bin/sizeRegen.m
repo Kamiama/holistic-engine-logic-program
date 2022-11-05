3dc52976565a1e487ed7333ee37c480601181994
Jan Ayala <89804849+Janayala@users.noreply.github.com>
2022-11-05 11:56:14 -0400

    Update sizeRegen.m

--- bin/sizeRegen.m
+++ bin/sizeRegen.m
@@ -36,16 +36,18 @@ g = 9.81; % gravitational accelaration [m/s]
 %% Calculate minimum wall thickness
 % determine optimal wall thickness based on throat conditions (because this is the most extreme location in the engine)
 
+%% CEA Inputs
+P_c = 300;
+P_e =
+
 % initial CEA run to find throat conditions (OUTPUTS IN METRIC)
-[~, ~, ~, M, gamma, P, T, rho, mu, Pr, Mw, k, son, cp] = RunCEA(P_c, P_e, fuel, fuel_weight, fuel_temp, oxidizer, oxidizer_temp, OF, 0, 0, CEA_input_name, 1, 0);
+[~, ~, ~, M, gamma, P, T, rho, mu, Pr_gas, Mw, k, son, cp] = RunCEA(P_c, P_e, fuel, fuel_weight, fuel_temp, oxidizer, oxidizer_temp, OF, 0, 0, CEA_input_name, 1, 0);
 
 % perform initial heat transfer and structural calculations to find wall thickness
-r = Pr ^ (1 / 3); % recovery factor - biased towards larger engines, very small engines should use Pr^.5 (Heister 196).
-T_r = T * (1 + r * (gamma - 1) / 2 * M ^ 2) / (1 * (gamma - 1) / 2 * M ^ 2); % recovery temp (adiabatic wall temp) - corrects for compressible boundry layers (Huzel & Huang 85).
 
 
-sigma = (.5 * T_wg / T_c * (1 + (gamma - 1) / 2 * M ^ 2) + .5) ^ -.68 * (1 + (gamma - 1) / 2 * M ^ 2) ^ -.12; % film coefficient correction factor (Huzel & Huang 86).
-h_g = (.026 / D_t ^ .2) * (mu ^ .2 * cp / Pr ^ .6) * (P_c * g / c_star) ^ .8 * (D_t / radius_throat) ^ .1 * (A_t / A_t) ^ .9 * sigma; % film coefficient - bartz equation (Huzel & Huang 86).
+
+
 q_dot = 
 
 %% Set inlet properties
@@ -62,7 +64,6 @@ m_dot_CHANNEL = m_dot / num_channels;
 
 subsonic_area_ratios = (pi * r_contour(x_contour < 0) .^ 2) / A_t;
 supersonic_area_ratios = (pi * r_contour(x_contour > 0) .^ 2) / A_t;
-[~, ~, ~, M, gamma, P, T, rho, mu, Pr, Mw, k, son] = RunCEA(P_c, P_e, fuel, fuel_weight, fuel_temp, oxidizer, oxidizer_temp, OF, 0, 0, CEA_input_name, 0);
 
 %% Begin iterative sizing
 % start at manifold and step up along chamber length
@@ -92,7 +93,7 @@ characteristic_length = 10 %Longest length of Channels
 
 
 %Coolant properties
-viscosity = 10; %Viscosity
+liq_mu = 10; %Viscosity
 Cp = 100; %Specific heat at constant pressure
 kc = 100; %Thermal Conductivity of Coolant
 Tl = 293;
@@ -114,22 +115,22 @@ numchannels = pi * (engine_diameter + 0.8 * (Dstar + 2 * wallthick)) / (Dstar + 
 mdotchan = mdotf / numchannels; %Mass flow of channel (EQ 6.31)
 %Step 3: Begin Stepping Down tube/channel
 for i = [1:stepsize]
-    [~, ~, ~, ~, gamma, P, T, density, mu, Pr_gas, Mw, k, son, cp] = RunCEA(P_c, P_e, fuel, fuel_weight, fuel_temp, oxidizer, oxidizer_temp, OF, 0, 0, CEA_input_name, 1, 0);
-
-
-    Pr_gas = densityfs * viscosityfs * D;
-    Pr_liquid = density(Tl(i));
+    [~, ~, ~, ~, gamma, P, T_gas, density, gas_mu, Pr_gas, Mw, k, son, cp] = RunCEA(P_c, P_e, fuel, fuel_weight, fuel_temp, oxidizer, oxidizer_temp, OF, 0, 0, CEA_input_name, 1, 0);
+    r = Pr ^ (1 / 3); % recovery factor - biased towards larger engines, very small engines should use Pr^.5 (Heister 196).
+    T_r = T_gas * (1 + r * (gamma - 1) / 2 * M ^ 2) / (1 * (gamma - 1) / 2 * M ^ 2); % recovery temp (adiabatic wall temp) - corrects for compressible boundry layers (Huzel & Huang 85).
+    liq_mu = 
+    Pr_liquid = liq_mu * Cp / kc;
     while liqheattransfer < gasheattransfer * upperbound || liqheattransfer > gasheattransfer * lowerbound
         %Step 5: Calculate Gas film coefficient and heat transfer 
-        hg = 0.026 / Dstar^0.2 * (viscosity^0.2 * Cp / prandtl^0.6) * ...
-        (chamber_pressure * g / Cstar)^0.8 * (Dstar / throat_radius_curvature)^0.1 * (Astar/Alocal[i])^0.9 * (meandens / freedens)^0.8 * (meanvisc / freevisc)^0.2; %Gas Film Coefficent Bartz 
-        gasheattransfer = heatcoef * (Tgas(i) - Twgi);  %Gas Heat Transfer (EQ 6.16)
+        sigma = (.5 * T_wg / T_c * (1 + (gamma - 1) / 2 * M ^ 2) + .5) ^ -.68 * (1 + (gamma - 1) / 2 * M ^ 2) ^ -.12; % film coefficient correction factor (Huzel & Huang 86).
+        h_g = (.026 / D_t ^ .2) * (mu ^ .2 * cp / Pr ^ .6) * (P_c * g / c_star) ^ .8 * (D_t / radius_throat) ^ .1 * (A_t / A_t) ^ .9 * sigma; % film coefficient - bartz equation (Huzel & Huang 86).
+        gasheattransfer = h_g * (T_r - Twgi);  %Gas Heat Transfer (EQ 6.16)
 
         %Step 6: Calculate Liquid Wall Temperature from conduction 
         Twl(i) = gaswalltemp - gasheattrans * wallthick / thermconduct; %Liquid Wall Temp (EQ 6.29)
 
         %Step 7: Compute Liquid Film Coefficient
-        hl = (a * re^m * pr^n * (visfree / viscwall)^b) * k / L;    %Liquid Film Coefficient (EQ 6.19) 
+        hl = (a * Re_liquid^m * Pr_liquid^n * (visfree / viscwall)^b) * k / L;    %Liquid Film Coefficient (EQ 6.19) 
 
         %Step 8: Compute Heat Flux. Compare it to step 5
         liqheatransfer = hl * (Tl(i) - Twl(i)); %Liquid Heat Transfer (EQ  6.29)
